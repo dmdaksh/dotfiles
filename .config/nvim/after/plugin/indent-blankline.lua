d0c2ecbc617d9a17db6cef34b47239de291d3882
Daksh Maheshwari <dmdaksh.97@gmail.com>
2022-01-01 23:15:02 +0530

    modified indent-blankline config to show eol

--- .config/nvim/after/plugin/indent-blankline.lua
+++ .config/nvim/after/plugin/indent-blankline.lua
@@ -1,8 +1,11 @@
 if packer_plugins['indent-blankline.nvim'] and
   packer_plugins['indent-blankline.nvim'].loaded then
+  vim.opt.list = true
+  vim.opt.listchars:append("eol:↴")
   require('indent_blankline').setup {
     char = "▏",
     show_first_indent_level = false,
+    show_end_of_line = true,
     filetype_exclude = {
       'help', 'git', 'markdown', 'text', 'terminal', 'lspinfo', 'packer'
     },
