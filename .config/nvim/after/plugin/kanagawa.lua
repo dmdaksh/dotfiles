6d9e70fc0d93c88bd112a7cb28f9a9cb9eb0470b
Daksh Maheshwari <dmdaksh.97@gmail.com>
2022-05-14 18:52:55 +0530

    added alacritty config

--- .config/nvim/after/plugin/kanagawa.lua
+++ .config/nvim/after/plugin/kanagawa.lua
@@ -10,7 +10,7 @@ if packer_plugins['kanagawa.nvim'] and packer_plugins['kanagawa.nvim'].loaded th
     variablebuiltinStyle = "italic",
     specialReturn = true, -- special highlight for the return keyword
     specialException = true, -- special highlight for exception handling keywords 
-    transparent = false, -- do not set background color
+    transparent = true, -- do not set background color
     colors = {},
     overrides = {}
   })
