5dcbe57f57d30c972d93456ae91b7efe6fbcf6cc
Daksh Maheshwari <dmdaksh.97@gmail.com>
2025-09-25 23:08:50 -0700

    This commit switches the colorscheme for both Kitty and Neovim from Rosé Pine to the Kanagawa dragon theme.
    The following changes are included:
    - Update kitty configuration to use the kanagawa theme
    - Update neovim colorscheme to kanagawa
    - Update lazyvim lockfile
    - Comment out unused autocmd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,11 +2,11 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd("BufEnter", {
-  callback = function()
-    vim.opt.formatoptions:remove({ "c", "r", "o" })
-  end,
-  desc = "Disable New Line Comment",
-})
+-- local autocmd = vim.api.nvim_create_autocmd
+--
+-- autocmd("BufEnter", {
+--   callback = function()
+--     vim.opt.formatoptions:remove({ "c", "r", "o" })
+--   end,
+--   desc = "Disable New Line Comment",
+-- })
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,14 @@
+-- base kanagawa theme
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("kanagawa-dragon")
+    end,
+  },
+}
+
 -- -- base rose-pine theme
 -- return {
 --   "rose-pine/neovim",
@@ -7,46 +18,46 @@
 --   end,
 -- }
 
--- custom rose-pine theme
-return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    -- make sure terminal colors are enabled
-    vim.opt.termguicolors = true
+-- -- custom rose-pine theme
+-- return {
+--   "rose-pine/neovim",
+--   name = "rose-pine",
+--   config = function()
+--     -- make sure terminal colors are enabled
+--     vim.opt.termguicolors = true
+--
+--     local rp = require("rose-pine")
+--     local cfg = require("rose-pine.config")
+--
+--     rp.setup({
+--       -- core defaults (satisfy the LSP)
+--       variant = cfg.variant,
+--       dark_variant = cfg.dark_variant,
+--       dim_inactive_windows = cfg.dim_inactive_windows,
+--       extend_background_behind_borders = cfg.extend_background_behind_borders,
+--       enable = cfg.enable,
+--       styles = cfg.styles,
+--       palette = cfg.palette,
+--       groups = cfg.groups,
+--       before_highlight = cfg.before_highlight,
+--
+--       -- my overrides
+--       disable_background = true,
+--       disable_float_background = true,
+--       highlight_groups = {
+--         Normal = { bg = "none" },
+--         NormalFloat = { bg = "none" },
+--         Folded = { bg = "none" },
+--         SignColumn = { bg = "none" },
+--         EndOfBuffer = { bg = "none" },
+--       },
+--     })
+--
+--     vim.cmd("colorscheme rose-pine")
+--   end,
+-- }
 
-    local rp = require("rose-pine")
-    local cfg = require("rose-pine.config")
-
-    rp.setup({
-      -- core defaults (satisfy the LSP)
-      variant = cfg.variant,
-      dark_variant = cfg.dark_variant,
-      dim_inactive_windows = cfg.dim_inactive_windows,
-      extend_background_behind_borders = cfg.extend_background_behind_borders,
-      enable = cfg.enable,
-      styles = cfg.styles,
-      palette = cfg.palette,
-      groups = cfg.groups,
-      before_highlight = cfg.before_highlight,
-
-      -- my overrides
-      disable_background = true,
-      disable_float_background = true,
-      highlight_groups = {
-        Normal = { bg = "none" },
-        NormalFloat = { bg = "none" },
-        Folded = { bg = "none" },
-        SignColumn = { bg = "none" },
-        EndOfBuffer = { bg = "none" },
-      },
-    })
-
-    vim.cmd("colorscheme rose-pine")
-  end,
-}
-
--- {
+-- return {
 --   {
 --     "LazyVim/LazyVim",
 --     opts = {
